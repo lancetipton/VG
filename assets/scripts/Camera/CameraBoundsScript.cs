80143eb4210bc4db3cf38679f0fb39b34c131197
Tom Palmer <tjpalmer@tjpalmer.com>
2016-01-30 11:35:01 -0700

    Bound multiple players with camera

--- assets/scripts/Camera/CameraBoundsScript.cs
+++ assets/scripts/Camera/CameraBoundsScript.cs
@@ -16,15 +16,31 @@ public class CameraBoundsScript: MonoBehaviour {
 	Vector3 startPos;
 
 	void Update() {
+		// Figure out our bounds and such.
 		var first = targets[0];
-		var bounds = new Bounds(first.transform.position, Vector3.zero);
+		var bounds = new Bounds(getCenter(first), Vector3.zero);
+		// This duplicates the first, but that shouldn't be a big deal.
 		foreach (var target in targets) {
-			bounds.Encapsulate(target.transform.position);
+			bounds.Encapsulate(getCenter(target));
 		}
 		var camera = GetComponent<Camera>();
+		// Make sure we are showing enough in both dimensions.
+		var scale = 1.5f;
+		var minSize = 4f;
 		var aspect = camera.aspect;
-		camera.orthographicSize = bounds.extents.y;
+		var sizeY = Mathf.Max(minSize, scale * bounds.extents.y);
+		var sizeX = aspect * sizeY;
+		if (sizeX < scale * bounds.extents.x) {
+			sizeY = scale * bounds.extents.x / aspect;
+        } else if (sizeX < minSize) {
+			sizeY = minSize / aspect;
+		}
+		// Set the values.
+		camera.orthographicSize = sizeY;
 		transform.position = new Vector3(bounds.center.x, bounds.center.y, transform.position.z);
 	}
 
+	private static Vector3 getCenter(GameObject obj) {
+		return obj.GetComponent<Renderer>().bounds.center;
+    }
 }
