02422339354e318a1099b0fe91b96bbe63cb9dd5
Lance Tipton <lancetipton04@gmail.com>
2016-01-30 11:41:22 -0700

    Added respwans.

--- assets/scripts/Level/Respawns.cs
+++ assets/scripts/Level/Respawns.cs
@@ -6,14 +6,14 @@ public class Respawns : MonoBehaviour {
 	public static Respawns instance;
 
 
-	public GameObject[] ResData = new GameObject[4];
+	public GameObject[] ResData = new GameObject[8];
 
 	void Awake(){
 		instance = this;
 	}
 
 	public GameObject RandomSpawn(){
-		int num = Random.Range(0,3);
+		int num = Random.Range(0,7);
 		return ResData[num];
 	}
 
