30d8f9e4fbb4ad9c14f799d23e35e3d78be69e75
Lance Tipton <lancetipton04@gmail.com>
2016-01-30 14:33:49 -0700

    Added lighting strike.

--- assets/scripts/Level/Lightning.cs
+++ assets/scripts/Level/Lightning.cs
@@ -4,14 +4,17 @@ using System.Collections;
 public class Lightning : MonoBehaviour {
 
 	public static Lightning instance;
-
+	public GameObject Flash;
 	public ParticleSystem[] Strikes = new ParticleSystem[5];
+
+	int count = 0;
 
 	float strikeTimer;
 	// -23 / 34
 	void Awake(){
 		instance = this;
 		strikeTimer = Time.time + Random.Range(1, 5);
+		Flash.SetActive(false);
 	}
 
 	// Use this for initialization
@@ -28,6 +31,7 @@ public class Lightning : MonoBehaviour {
 
 	void PlayStrike(){
 		PlayLighting();
+		PlayFlash();
 		strikeTimer = Time.time + Random.Range(1, 5);
 	}
 
@@ -38,4 +42,24 @@ public class Lightning : MonoBehaviour {
 		Strikes[number].Play();
 	}
 
+	void PlayFlash(){
+		if(count < 3){
+			count += 1;
+			FlashOn();
+		}
+		else{
+			Flash.SetActive(false);
+			count = 0;
+		}
+	}
+
+	void FlashOn(){
+		Flash.SetActive(true);
+		Invoke("FlashOff", 0.1f);
+	}
+
+	void FlashOff(){
+		Flash.SetActive(false);
+		Invoke("PlayFlash", 0.1f);
+	}
 }
