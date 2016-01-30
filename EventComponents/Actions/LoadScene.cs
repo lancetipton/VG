ab58b78687fc8194162802bfb7bdc443c77c0d3e
JoeStrout <joe@strout.net>
2016-01-29 22:53:51 -0700

    Added more boilerplate scripts (EventComponents), plus current version of the player controller.

---
+++ EventComponents/Actions/LoadScene.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+using System.Collections;
+
+public class LoadScene : MonoBehaviour {
+	[Tooltip("Invoked periodically while level loads; receives progress in range 0-1.")]
+	public FloatEvent progress;
+
+	[Tooltip("Invoked when the loading of the level is complete.")]
+	public UnityEvent complete;
+
+	/// <summary>
+	/// Start loading the specified scene (asynchronously).
+	/// </summary>
+	/// <param name="sceneName">Scene name.</param>
+	public void Load(string sceneName) {
+		StartCoroutine(LoadAsync(sceneName));
+	}
+
+	/// <summary>
+	/// Coroutine to load the scene asynchronously, invoking the
+	/// appropriate callbacks.
+	/// </summary>
+	/// <param name="sceneName">Scene name.</param>
+	IEnumerator LoadAsync(string sceneName) {
+		AsyncOperation async = Application.LoadLevelAsync(sceneName);
+		while (!async.isDone) {
+			if (progress != null) progress.Invoke(async.progress);
+			yield return null;
+		}
+		if (complete != null) complete.Invoke();
+	}
+}
