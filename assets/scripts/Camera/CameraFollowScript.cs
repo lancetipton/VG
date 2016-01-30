47cf52c2de97196ef29f9b34b71e40d656326cfa
JoeStrout <joe@strout.net>
2016-01-30 09:28:41 -0700

    Added whole project (assets and ProjectSettings).

---
+++ assets/scripts/Camera/CameraFollowScript.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraFollowScript : MonoBehaviour {
+
+  public static CameraFollowScript instance;
+
+  public GameObject target;
+
+  public float offsetX;
+  public float offsetY;
+
+
+  private float posX;
+  private float posY;
+  private float posZ;
+
+  Vector3 startPos;
+
+
+  void Awake(){
+    instance = this;
+  }
+
+
+  void Update () {
+
+      posX = target.transform.position.x + offsetX;
+      posY = target.transform.position.y + offsetY;
+      posZ = transform.position.z;
+
+      transform.position = new Vector3 (posX, posY, posZ);
+
+  }
+
+
+
+
+}
---
+++ assets/scripts/EventComponents/UnityEventSubclasses.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+[System.Serializable]
+public class StringEvent : UnityEvent<string> {}
+
+[System.Serializable]
+public class FloatEvent : UnityEvent<float> {}
+
+[System.Serializable]
+public class IntEvent : UnityEvent<int> {}
+
+[System.Serializable]
+public class GameObjectEvent : UnityEvent<GameObject> {}
+
+[System.Serializable]
+public class Vector3Event : UnityEvent<Vector3> {}
