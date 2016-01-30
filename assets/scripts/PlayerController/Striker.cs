6089bb2ae51c074965d0d19e81799f2c58bd051a
JoeStrout <joe@strout.net>
2016-01-30 11:31:04 -0700

    Reworked how damage is conveyed from the fist (or other striker) to the target. Added a simple UI for displaying player status.

---
+++ assets/scripts/PlayerController/Striker.cs
@@ -0,0 +1,48 @@
+﻿/*
+This script attached to the character's striking surface
+(fist, weapon, etc.).  It applies damage to whatever's hit.
+*/
+using UnityEngine;
+using UnityEngine.Events;
+using System.Collections.Generic;
+
+public class Striker : MonoBehaviour {
+	#region Public Properties
+
+	#endregion
+	//--------------------------------------------------------------------------------
+	#region Private Properties
+
+	#endregion
+	//--------------------------------------------------------------------------------
+	#region MonoBehaviour Events
+	void Start() {
+	
+	}
+	
+	void Update() {
+	
+	}
+	
+	protected void OnTriggerEnter2D(Collider2D other) {
+		Vector2 force = transform.TransformDirection(Vector3.right);
+		
+		Rigidbody2D targetRbody = other.GetComponent<Rigidbody2D>();
+		if (targetRbody != null) targetRbody.AddForceAtPosition(force, 
+				transform.position, ForceMode2D.Impulse);
+		
+		CharDamage dam = other.GetComponent<CharDamage>();
+		if (dam != null) dam.ApplyDamage(10);
+	}
+	
+
+	#endregion
+	//--------------------------------------------------------------------------------
+	#region Public Methods
+	
+	#endregion
+	//--------------------------------------------------------------------------------
+	#region Private Methods
+
+	#endregion
+}
---
+++ assets/scripts/UI/PlayerStatusDisplay.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
+using System.Collections.Generic;
+
+public class PlayerStatusDisplay : MonoBehaviour {
+	#region Public Properties
+	
+	public CharDamage charDamage;
+	
+	#endregion
+	//--------------------------------------------------------------------------------
+	#region Private Properties
+	
+	Text playerNameText;
+	Text damageText;
+	int lastDamageShown = -1;
+	
+	#endregion
+	//--------------------------------------------------------------------------------
+	#region MonoBehaviour Events
+	void Start() {
+		playerNameText = transform.FindChild("PlayerNameText").GetComponent<Text>();
+		damageText = transform.FindChild("DamageText").GetComponent<Text>();
+		playerNameText.text = charDamage.gameObject.name;
+	}
+	
+	void Update() {
+		int dam = charDamage.damage;
+		if (dam != lastDamageShown) {
+			damageText.text = charDamage.damage.ToString();
+			lastDamageShown = dam;
+		}
+	}
+
+	#endregion
+	//--------------------------------------------------------------------------------
+	#region Public Methods
+	
+	#endregion
+	//--------------------------------------------------------------------------------
+	#region Private Methods
+
+	#endregion
+}
