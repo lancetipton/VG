45d8c418d49cf389de0b6c91520d23c26eef7ab2
Lance Tipton <lancetipton04@gmail.com>
2016-01-30 19:17:10 -0700

    Added indicator.

--- assets/scripts/UI/FollowPlayer.cs
+++ assets/scripts/UI/FollowPlayer.cs
@@ -4,25 +4,27 @@ using System.Collections;
 public class FollowPlayer : MonoBehaviour {
 
 	public Transform player;
-	public bool active = false;
+	public bool active = true;
 	public float offset = 3f;
-	// Use this for initialization
 	
-  void Update () {
-  	TurnOn();
-  }
+	Vector3 our_scale;
 
-  public void TurnOn(){
+	void Start(){
+		our_scale = transform.localScale;
+	}
 
-  	Vector3 pos = new Vector3(player.position.x, player.position.y +offset, player.position.z);
-  	Vector3 screenPos = Camera.main.WorldToScreenPoint(pos);
-    transform.position = new Vector3 (screenPos.x, screenPos.y + offset, screenPos.z);
-
-  	//Invoke("TurnOff", 3f);
-  }
-
-  void TurnOff(){
-
-  }
+	void LateUpdate(){
+		if(active){
+			if(player.localScale.x < 0){
+				transform.localScale = new Vector3(-our_scale.x,transform.localScale.y, transform.localScale.z);
+			}
+			else{
+				transform.localScale = our_scale;
+			}
+			Vector3 pos = new Vector3(player.position.x, player.position.y +offset, player.position.z);
+			Vector3 screenPos = Camera.main.WorldToScreenPoint(pos);
+			transform.position = new Vector3 (screenPos.x, screenPos.y + offset, screenPos.z);
+		}
+	}
 
 }
