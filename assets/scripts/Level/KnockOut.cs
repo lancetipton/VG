803a16d4d1e6d8ea0ec5cc2cfd3c0a743d20e0ec
Tom Palmer <tjpalmer@tjpalmer.com>
2016-01-30 19:36:14 -0700

    Lose goat on knockout

--- assets/scripts/Level/KnockOut.cs
+++ assets/scripts/Level/KnockOut.cs
@@ -5,6 +5,7 @@ public class KnockOut : MonoBehaviour {
 
 	void OnTriggerEnter2D(Collider2D coll) {
 		if (coll.gameObject.tag == "Players") {
+			coll.gameObject.GetComponentInChildren<GoatGrabber>().DropGoat(new Vector2());
 			coll.gameObject.GetComponent<RespawnPlayer>().RestPos();
 		} else if (coll.gameObject.tag == "Goat") {
 			coll.gameObject.GetComponent<Goat>().Respawn();
