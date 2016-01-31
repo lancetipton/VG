68370047e7ce831ba81f6d8258ffcba375306901
Lance Tipton <lancetipton04@gmail.com>
2016-01-31 12:13:25 -0700

    Started Audio.

---
+++ assets/scripts/GameMaster/SoundManager.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SoundManager : MonoBehaviour {
+
+	public static SoundManager instance;
+	public AudioSource MusicPlayer;
+	public AudioSource[] FxPlayer = new AudioSource[4];
+
+	public AudioClip[] Music = new AudioClip[2];
+	public AudioClip[] soundFx = new AudioClip[30];
+
+
+	void Awake(){
+		if(instance == null)
+		{
+		  DontDestroyOnLoad(gameObject);
+		  instance = this;
+		}
+		else if(instance != this) {
+		  Destroy(gameObject);
+		}
+	}
+
+
+	public void FindFX(int playerNum, string fx){
+		if(fx == "jump"){
+
+		}
+		else if(fx == "hard"){
+
+		}
+		else if(fx == "soft"){
+
+		}
+		else if(fx == "hard"){
+
+		}
+		else if(fx == "pick-up"){
+
+		}
+		else if(fx == "grunt"){
+
+		}
+	}
+
+
+	void PlayFX(int playerNum, int fxNum){
+		FxPlayer[playerNum].PlayOneShot(soundFx[fxNum]);
+	}
+
+	public void PlayMusic(int type){
+		MusicPlayer.clip = Music[type];
+		MusicPlayer.Play();
+	}
+
+}
